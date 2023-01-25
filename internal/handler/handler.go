86dc4889b01f580fe9eaacf8a013cb8629323c88
Allayar07 <allayarhudaygulyyev14@gmail.com>
2023-01-25 19:31:01 +0500

    ignored v3

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -20,9 +20,17 @@ func (h *Handler) InitRoutes() *gin.Engine {
 
 	api := routes.Group("/api")
 	{
+		//endpoint for upload files into uploads folder
 		api.POST("/uploads", h.Download)
+
+		//endpoint for delete files from uploads folder
 		api.DELETE("/uploads/:any", h.Delete)
+
+		//endpoint for opening files
 		api.GET("/uploads/open/:any", h.OpenFile)
+
+		//endpoint for exchange image formats. for example: convert jpg format to png format,
+		//format of images taken from query params
 		api.GET("/uploads/get/:any", h.ChangeIMG)
 	}
 
